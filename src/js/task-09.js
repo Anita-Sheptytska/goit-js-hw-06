db63315da9933f1686adbe97787d172110e42350
Anita-Sheptytska <anita.sheptytska@gmail.com>
2022-12-11 19:54:59 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,3 +2,20 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const btnEl = document.querySelector('.change-color');
+console.log(btnEl);
+
+const spanEl = document.querySelector('.color');
+console.log(spanEl);
+
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = bodyEl.style.backgroundColor;
+}
+
+btnEl.addEventListener("click", changeColor);
+
+
